9b323cb1d244025e50ccc558cdce6ba7181f9248
Juan Arquero <j.arquerog@gmail.com>
2023-10-26 09:50:22 +0200

    Read Analog

---
+++ PROGRAMAS PLANTILLA/ESP32/ReadAnalogValue.ino
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+// Analog Sensor is connected to GPIO 34 (Analog ADC1_CH6) 
+const int sensorPin = 34;
+
+// variable for storing the potentiometer value
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  // Reading sensor value
+  sensorValue = analogRead(sensorPin);
+  Serial.println("Sensor analog value: ");
+  Serial.print(sensorValue);
+  delay(2000);
+}
